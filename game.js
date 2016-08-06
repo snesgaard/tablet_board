b9c5e4fd1e56868201c4dfb74f1cec997a92c4ae
Sebastian Nesgaard Jensen <s.nesgaard@gmail.com>
2016-08-06 16:01:40 +0200

    Added functionality for freezing game and warning users when tablet is turned the wrong way

--- game.js
+++ game.js
@@ -1,10 +1,56 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+window.onload = function() {
+	var gameRatio = window.innerWidth/window.innerHeight;
+	var game = new Phaser.Game(Math.ceil(640*gameRatio), 640, Phaser.CANVAS);
+	var firstRunLandscape;
+	var play = function(game){}
 
-function preload() {
-}
 
-function create() {
-}
 
-function update() {
+	play.prototype = {
+
+		preload:function(){
+
+			firstRunLandscape = game.scale.isGameLandscape;
+
+			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+			game.scale.forceOrientation(false, true);
+
+			game.scale.enterIncorrectOrientation.add(handleIncorrect);
+
+      game.scale.leaveIncorrectOrientation.add(handleCorrect);
+
+		},
+
+		create:function(){
+
+		}
+
+	}
+
+
+
+	function handleIncorrect(){
+     	if(!game.device.desktop){
+     		document.getElementById("turn").style.display="block";
+     	}
+	}
+
+
+
+	function handleCorrect(){
+		if(!game.device.desktop){
+			if(firstRunLandscape){
+				gameRatio = window.innerWidth/window.innerHeight;
+				game.width = Math.ceil(640*gameRatio);
+				game.height = 640;
+				game.renderer.resize(game.width,game.height);
+				game.state.start("Play");
+			}
+			document.getElementById("turn").style.display="none";
+		}
+	}
+
+	game.state.add("Play",play);
+	game.state.start("Play");
 }
